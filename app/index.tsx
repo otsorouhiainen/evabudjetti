b98787d932da82b2ad3f6eefaeeccc5ba0b2bca1
Juliusz Kotelba <juliusz.kotelba@gmail.com>
2025-10-04 20:26:19 +0300

    Added landing page and theme colors

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { ButtonAccessoriesShowcase } from '@/src/components/ButtonExample';
 import * as eva from '@eva-design/eva';
 import {
 	ApplicationProvider,
@@ -7,7 +8,6 @@ import {
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { Link } from 'expo-router';
 import { View } from 'react-native';
-import { ButtonAccessoriesShowcase } from '@/src/components/ButtonExample';
 
 export default function Index() {
 	return (
---
+++ src/theme/eva-theme.tsx
@@ -0,0 +1,20 @@
+export const customTheme: Record<string, string> = {
+  'color-primary-100': '#0A5B55',
+  'color-primary-200': '#0E776E',
+  'color-primary-300': '#14968A',
+  'color-primary-400': '#74A57F',
+  'color-primary-500': '#9ECE9A',
+  'color-primary-600': '#C3EAC0',
+
+  'color-secondary-100': '#E4C5AF',
+  'color-secondary-200': '#F4E3D3',
+
+  'color-success-500': '#4CAF50',
+  'color-warning-500': '#FFC107',
+  'color-danger-500': '#F44336',
+  'color-info-500': '#2196F3',
+
+  'color-black': '#2F2F2F',
+  'color-white': '#fdfdfd', 
+    // Add more custom colors as needed
+};
