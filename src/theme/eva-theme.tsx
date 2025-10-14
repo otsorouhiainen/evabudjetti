3a4674c0584e558877fd912e4262657bef8d96a5
Juliusz Kotelba <juliusz.kotelba@gmail.com>
2025-10-15 02:28:54 +0300

    Fixed pr according to Aki's comments

--- src/theme/eva-theme.tsx
+++ src/theme/eva-theme.tsx
@@ -1,20 +1,24 @@
 export const customTheme: Record<string, string> = {
-  'color-primary-100': '#0A5B55',
-  'color-primary-200': '#0E776E',
-  'color-primary-300': '#14968A',
-  'color-primary-400': '#74A57F',
-  'color-primary-500': '#9ECE9A',
-  'color-primary-600': '#C3EAC0',
+	'color-primary-100': '#0A5B55',
+	'color-primary-200': '#0E776E',
+	'color-primary-300': '#14968A',
+	'color-primary-400': '#74A57F',
+	'color-primary-500': '#9ECE9A',
+	'color-primary-600': '#C3EAC0',
 
-  'color-secondary-100': '#E4C5AF',
-  'color-secondary-200': '#F4E3D3',
+	'color-secondary-100': '#E4C5AF',
+	'color-secondary-200': '#F4E3D3',
 
-  'color-success-500': '#4CAF50',
-  'color-warning-500': '#FFC107',
-  'color-danger-500': '#F44336',
-  'color-info-500': '#2196F3',
+	'color-success-500': '#4CAF50',
+	'color-warning-500': '#FFC107',
+	'color-danger-500': '#F44336',
+	'color-info-500': '#2196F3',
 
-  'color-black': '#2F2F2F',
-  'color-white': '#fdfdfd', 
-    // Add more custom colors as needed
+	'color-black': '#2F2F2F',
+	'color-white': '#fdfdfd',
+
+	'color-button-bg': '#F1F5F4',
+	'color-segment-wrap': '#dff1eb',
+
+	// Add more custom colors as needed
 };
