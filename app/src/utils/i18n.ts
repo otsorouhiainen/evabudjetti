41efc7d2dc12f17fe0dadcf8b1fd82ed9c0045e0
Elina <elinamariaraut@gmail.com>
2025-10-31 23:01:52 +0200

    Add i18next, translation file and translation examples

---
+++ app/src/utils/i18n.ts
@@ -0,0 +1,29 @@
+import i18next from 'i18next';
+
+i18next.init({
+    lng: 'fi',
+    debug: true,
+    resources: {
+        en: {
+            translation: {
+                "Test string": "Test string",
+                "Test string with interpolated value {{value}}": "Test string with interpolated value {{value}}"
+            }
+        },
+        fi: {translation: {
+            "Test string": "Testistringi",
+            "Test string with interpolated value {{value}}": "Testistringi interpoloidulla arvolla {{value}}"
+        }}
+    }
+})
+
+/*  
+/   Basic use example (when language is 'fi')
+/   console.log(i18next.t("Test string")) <- This would print "Testistringi"
+*/
+
+/*  
+/   Interpolation use example (when language is 'fi')
+/   const testVariable = 5
+/   console.log(i18next.t("Test string with interpolated value {{value}}", { value: testVariable } )) <- This would print "Testistringi interpoloidulla arvolla 5"
+*/
