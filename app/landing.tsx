a471aa77e330ae3b9738325d02acbee3a7725623
Juliusz Kotelba <juliusz.kotelba@gmail.com>
2025-11-02 19:49:40 +0200

    Fixed to use tamagui

--- app/landing.tsx
+++ app/landing.tsx
@@ -1,6 +1,7 @@
 import { MessageCircleQuestion, PiggyBank } from '@tamagui/lucide-icons';
 import { useRouter } from 'expo-router';
 import { useState } from 'react';
+import { ScrollView } from 'react-native';
 import { Button, SizableText, Stack, XStack, YStack } from 'tamagui';
 
 export default function Landing() {
@@ -17,7 +18,7 @@ export default function Landing() {
 					bottom={0}
 					left={0}
 					right={0}
-					backgroundColor="rgba(0,0,0,0.4)"
+					backgroundColor="$black"
 					justifyContent="center"
 					alignItems="center"
 					zIndex={10}
@@ -31,19 +32,30 @@ export default function Landing() {
 						padding={24}
 						width="80%"
 						minWidth={220}
-						maxWidth={400}
 						shadowColor="$black"
 						shadowOffset={{ width: 0, height: 2 }}
 						shadowOpacity={0.18}
 						shadowRadius={8}
 						elevation={8}
-						gap={'$3'}
+						gap={'$4'}
 					>
-						<SizableText size={'$title1'} marginBottom={8}>
+						<SizableText
+							size={'$title1'}
+							marginBottom={8}
+							fontFamily="$heading"
+						>
 							Ohjeet
 						</SizableText>
-						<SizableText size={'$title2'} marginBottom={16}>
-							Tervetuloa budjettisovellukseen! ...
+						<SizableText
+							size={'$title2'}
+							marginBottom={16}
+							fontFamily="$body"
+						>
+							Tervetuloa budjettisovellukseen! Käytä alavalikon
+							painikkeita siirtyäksesi eri osioihin. Lisää tulo
+							tai meno painamalla "Lisää tulo/meno". Voit
+							tarkastella ja muokata budjettiasi sekä nähdä
+							yhteenvedon taloudestasi.
 						</SizableText>
 						<Button
 							onPress={() => setHelpVisible(false)}
@@ -58,160 +70,189 @@ export default function Landing() {
 					</YStack>
 				</Stack>
 			)}
-			<YStack
-				flex={1}
-				justifyContent="center"
-				alignItems="center"
-				overflow="scroll"
-			>
-				<YStack
-					flex={1}
-					paddingTop={24}
-					paddingHorizontal={20}
-					gap={18}
-					maxWidth={600}
+			<YStack flex={1}>
+				<ScrollView
+					contentContainerStyle={{
+						flexGrow: 1,
+						alignItems: 'center',
+					}}
 				>
-					{/* Header */}
 					<YStack
-						alignItems="center"
-						marginTop={6}
-						justifyContent="space-between"
-						gap={'$2'}
+						flex={1}
+						paddingTop={24}
+						paddingHorizontal={20}
+						gap={18}
+						width="100%"
+						maxWidth={800}
 					>
-						<SizableText fontWeight={'$7'} size={'$title1'}>
-							EVA OmaBudjetti
-						</SizableText>
-						<SizableText
-							size={'$title2'}
-							hoverStyle={{ cursor: 'help' }}
+						{/* Header */}
+						<YStack
+							alignItems="center"
+							marginTop={6}
+							justifyContent="space-between"
+							gap={'$2'}
 						>
-							Taloudenhallintasi tueksi
-						</SizableText>
-						<Button
-							disabled
-							transparent
-							icon={<PiggyBank />}
-							size={200}
-							marginBottom={-190}
-							color={'$primary300'}
-						></Button>
-					</YStack>
-
-					{/* Illustration row */}
-					<XStack
-						alignItems="center"
-						justifyContent="center"
-						marginTop={4}
-						position="relative"
-						width="40%"
-						aspectRatio={1.1}
-						minWidth={80}
-						maxWidth={180}
-						alignSelf="center"
-					>
-						<Button
-							position="absolute"
-							top={0}
-							right={-80}
-							width="50%"
-							aspectRatio={1}
-							minWidth={28}
-							maxWidth={42}
-							zIndex={5}
-							onPress={() => setHelpVisible(true)}
-							chromeless
-							icon={<MessageCircleQuestion size={28} />}
-						/>
-						<Button disabled transparent />
-					</XStack>
-
-					{/* Balance card */}
-					<YStack
-						alignSelf="center"
-						width="88%"
-						borderRadius={16}
-						alignItems="center"
-						paddingVertical={16}
-						gap={6}
-						backgroundColor={'$white'}
-						shadowColor={'$black'}
-						shadowOffset={{ width: 0, height: 2 }}
-						shadowOpacity={0.15}
-						shadowRadius={8}
-						elevation={6}
-					>
-						<SizableText
-							size={'$title2'}
-							fontWeight={'$5'}
-							marginBottom={4}
-						>
-							04.10.2025
-						</SizableText>
-						<YStack>
-							<SizableText size={'$title2'}>
-								Tilillä rahaa:{' '}
-								<SizableText size={'$title3'} fontWeight={'$4'}>
-									1234€
-								</SizableText>
+							<SizableText
+								fontWeight={'$7'}
+								size={'$title1'}
+								fontFamily="$heading"
+							>
+								EVA OmaBudjetti
 							</SizableText>
-							<SizableText size={'$title2'}>
-								Käyttövara:{' '}
-								<SizableText size={'$title3'} fontWeight={'$4'}>
-									123€
-								</SizableText>
+							<SizableText
+								size={'$title2'}
+								fontFamily="$body"
+								hoverStyle={{ cursor: 'help' }}
+							>
+								Taloudenhallintasi tueksi
 							</SizableText>
+							<Button
+								disabled
+								transparent
+								icon={<PiggyBank />}
+								size={200}
+								marginBottom={-190}
+								color={'$primary300'}
+							></Button>
 						</YStack>
-						<Button
-							marginTop={10}
+
+						{/* Illustration row */}
+						<XStack
+							alignItems="center"
+							justifyContent="center"
+							marginTop={16}
+							position="relative"
 							alignSelf="center"
-							backgroundColor={'$primary500'}
-							color={'$white'}
-							padding={22}
-							size={32}
+							width={200}
+							height={120}
 						>
-							TARKASTELE
+							<Button
+								position="absolute"
+								top={0}
+								right="-10%"
+								width="50%"
+								aspectRatio={1}
+								minWidth={28}
+								maxWidth={42}
+								zIndex={5}
+								onPress={() => setHelpVisible(true)}
+								circular
+								size="$4"
+								chromeless
+								icon={<MessageCircleQuestion size={28} />}
+							/>
+						</XStack>
+
+						{/* Balance card */}
+						<YStack
+							alignSelf="center"
+							width="90%"
+							maxWidth={480}
+							borderRadius={16}
+							alignItems="center"
+							paddingVertical={16}
+							gap={6}
+							backgroundColor={'$white'}
+							shadowColor={'$black'}
+							shadowOffset={{ width: 0, height: 2 }}
+							shadowOpacity={0.15}
+							shadowRadius={8}
+							elevation={6}
+						>
+							<SizableText
+								size={'$title2'}
+								fontWeight={'$5'}
+								marginBottom={4}
+								fontFamily="$heading"
+							>
+								04.10.2025
+							</SizableText>
+							<YStack>
+								<SizableText
+									size={'$title2'}
+									fontFamily="$body"
+								>
+									Tilillä rahaa:{' '}
+									<SizableText
+										size={'$title3'}
+										fontWeight={'$4'}
+										fontFamily="$body"
+									>
+										1234€
+									</SizableText>
+								</SizableText>
+								<SizableText
+									size={'$title2'}
+									fontFamily="$body"
+								>
+									Käyttövara:{' '}
+									<SizableText
+										size={'$title3'}
+										fontWeight={'$4'}
+										fontFamily="$body"
+									>
+										123€
+									</SizableText>
+								</SizableText>
+							</YStack>
+							<Button
+								marginTop={10}
+								alignSelf="center"
+								backgroundColor={'$primary500'}
+								color={'$white'}
+								fontFamily={'$body'}
+								size={'$4'}
+								padding={22}
+							>
+								TARKASTELE
+							</Button>
+						</YStack>
+
+						{/* Primary CTA */}
+						<Button
+							size="$4"
+							marginTop={8}
+							borderRadius={28}
+							paddingVertical={20}
+							backgroundColor="$primary300"
+							onPress={() => router.push('/add_transaction')}
+						>
+							<SizableText
+								fontFamily="$body"
+								fontWeight="400"
+								color="$white"
+							>
+								LISÄÄ TULO/MENO
+							</SizableText>
 						</Button>
+
+						{/* Secondary CTAs */}
+						<XStack gap={14} justifyContent="space-between">
+							<Button
+								flex={1}
+								size={'$4'}
+								borderRadius={18}
+								padding={20}
+								backgroundColor={'$primary300'}
+								color={'$white'}
+								onPress={() => router.push('/budget')}
+							>
+								NÄYTÄ BUDJETTI
+							</Button>
+							<Button
+								flex={1}
+								size={'$4'}
+								borderRadius={18}
+								padding={20}
+								color={'$white'}
+								backgroundColor={'$primary300'}
+							>
+								MUOKKAA BUDJETTIA
+							</Button>
+						</XStack>
+						<YStack height={48} />
 					</YStack>
-
-					{/* Primary CTA */}
-					<Button
-						size="$4"
-						marginTop={8}
-						borderRadius={28}
-						paddingVertical={20}
-						backgroundColor="$primary300"
-						color="$white"
-						onPress={() => router.push('/add_transaction')}
-					>
-						LISÄÄ TULO/MENO
-					</Button>
-
-					{/* Secondary CTAs */}
-					<XStack gap={14} justifyContent="space-between">
-						<Button
-							flex={1}
-							size={'$4'}
-							borderRadius={18}
-							padding={20}
-							backgroundColor={'$primary300'}
-							color={'$white'}
-							onPress={() => router.push('/budget')}
-						>
-							NÄYTÄ BUDJETTI
-						</Button>
-						<Button
-							flex={1}
-							size={'$4'}
-							borderRadius={18}
-							padding={20}
-							color={'$white'}
-							backgroundColor={'$primary300'}
-						>
-							MUOKKAA BUDJETTIA
-						</Button>
-					</XStack>
-					<YStack height={48} />
-				</YStack>
+				</ScrollView>
 			</YStack>
 		</>
 	);
