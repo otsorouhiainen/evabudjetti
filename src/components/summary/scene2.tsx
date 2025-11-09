cf4c3fa6f2319834d4a2330e8dd2851fc3b42e0b
anssi <2003amt@gmail.com>
2025-11-09 19:58:38 +0200

    add CSS to second scene

--- src/components/summary/scene2.tsx
+++ src/components/summary/scene2.tsx
@@ -1,13 +1,25 @@
-import { Text, View } from 'react-native';
+import { PiggyBank } from '@tamagui/lucide-icons';
+import { Button, Text, View } from 'tamagui';
 
 export const Scene2 = ({ balance }: { balance: number }) => {
 	return (
-		<View>
-			<View>
-				<Text> Remaining </Text>
-
-				<Text> {balance} </Text>
-			</View>
+		<View alignItems="center" alignContent="center">
+			<Text fontSize={24} textAlign="center">
+				{' '}
+				Remaining{' '}
+			</Text>
+			<Text fontSize={36} textAlign="center">
+				{' '}
+				{balance}€{' '}
+			</Text>
+			<Button
+				disabled
+				transparent
+				marginTop={-84}
+				icon={<PiggyBank />}
+				size={400}
+				color={'$primary300'}
+			/>
 		</View>
 	);
 };
