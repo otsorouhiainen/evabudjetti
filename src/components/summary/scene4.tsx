c60ee8a81663ab578e193ca1e211de187233f2a9
anssi <2003amt@gmail.com>
2025-11-11 16:40:19 +0200

    add css to fourth scene

--- src/components/summary/scene4.tsx
+++ src/components/summary/scene4.tsx
@@ -1,26 +1,72 @@
-import { Text, View } from 'react-native';
+import type Expense from '@/app/summary';
+import { StyleSheet } from 'react-native';
+import { Text, View, XStack } from 'tamagui';
 
 export const Scene4 = ({
 	categories,
 	upcoming,
-}: { categories: string[]; upcoming: number[] }) => {
+}: { categories: string[]; upcoming: Expense[] }) => {
+	const font = 12;
+
 	return (
 		<View>
-			<Text> Top 5 categories by spend </Text>
-			<View>
-				{' '}
-				{categories.map((e) => (
-					<Text key={e}> {e} </Text>
-				))}{' '}
+			<View style={styles.container} padding={16} marginBottom={16}>
+				<Text> Top 5 categories by spend </Text>
+				<View>
+					{' '}
+					{categories.slice(0, 5).map((e, index) => (
+						<Text
+							style={styles.subContainer}
+							key={e}
+							fontSize={font}
+						>
+							{index + 1}. {e}{' '}
+						</Text>
+					))}{' '}
+				</View>
 			</View>
 
-			<Text> Upcoming expenses </Text>
-			<View>
-				{' '}
+			<View style={styles.container} padding={16} marginBottom={16}>
+				<Text> Upcoming expenses </Text>
 				{upcoming.map((e) => (
-					<Text key={e}> {e} </Text>
-				))}{' '}
+					<XStack
+						style={styles.subContainer}
+						key={e.name}
+						width="100%"
+					>
+						<Text width="33.33%" fontSize={font}>
+							{e.name}
+						</Text>
+
+						<Text width="33.33%" fontSize={font} textAlign="center">
+							{e.date}
+						</Text>
+
+						<Text width="33.33%" fontSize={font} textAlign="right">
+							{e.amount}
+						</Text>
+					</XStack>
+				))}
 			</View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#D3D3D3',
+		outlineColor: '#000000',
+		borderWidth: 2,
+		borderRadius: 10,
+		gap: 10,
+	},
+	subContainer: {
+		backgroundColor: '#eaeaea',
+		outlineColor: '#000000',
+		borderWidth: 1,
+		borderRadius: 8,
+		padding: 4,
+		marginBottom: 8,
+		gap: 10,
+	},
+});
