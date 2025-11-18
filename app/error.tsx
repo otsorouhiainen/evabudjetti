7a72ae97203518d4669c13936884c4a496227eab
anssi <2003amt@gmail.com>
2025-11-18 18:50:01 +0200

    format

--- app/error.tsx
+++ app/error.tsx
@@ -1,62 +1,52 @@
 import { PiggyBank } from '@tamagui/lucide-icons';
 import { useRouter } from 'expo-router';
 import i18next from 'i18next';
-import { Button, SizableText, Stack, XStack, YStack } from 'tamagui';
+import { Button, SizableText, YStack } from 'tamagui';
 
 export default function Err() {
 	const router = useRouter();
 
 	return (
-		<>
+		<YStack
+			flex={1}
+			justifyContent="center"
+			alignItems="center"
+			overflow="scroll"
+		>
+			{/* Header */}
 			<YStack
-				flex={1}
-				justifyContent="center"
 				alignItems="center"
-				overflow="scroll"
+				marginTop={6}
+				justifyContent="space-between"
+				gap={'$2'}
 			>
-				<YStack
-					flex={1}
-					paddingTop={24}
-					paddingHorizontal={20}
-					gap={18}
-					maxWidth={600}
-				>
-					{/* Header */}
-					<YStack
-						alignItems="center"
-						marginTop={6}
-						justifyContent="space-between"
-						gap={'$2'}
-					>
-						<Button
-							disabled
-							transparent
-							icon={<PiggyBank />}
-							size={400}
-							marginBottom={-80}
-							color={'$primary100'}
-						/>
-						<SizableText size={'$title2'}>404</SizableText>
-						<SizableText size={'$title2'} marginBottom={16}>
-							{i18next.t('Something went wrong.')}
-						</SizableText>
-					</YStack>
-					{/* Primary CTA */}
-
-					<Button
-						size="$4"
-						marginTop={8}
-						borderRadius={28}
-						paddingVertical={20}
-						backgroundColor={'$primary200'}
-						color="$white"
-						onPress={() => router.push('/landing')}
-					>
-						{i18next.t('Back to home')}
-					</Button>
-					<YStack height={48} />
-				</YStack>
+				<Button
+					disabled
+					transparent
+					icon={<PiggyBank />}
+					size={400}
+					marginBottom={-80}
+					color={'$primary100'}
+				/>
+				<SizableText size={'$title2'}>404</SizableText>
+				<SizableText size={'$title2'} marginBottom={16}>
+					{i18next.t('Something went wrong.')}
+				</SizableText>
 			</YStack>
-		</>
+			{/* Primary CTA */}
+
+			<Button
+				size="$4"
+				marginTop={8}
+				borderRadius={28}
+				paddingVertical={20}
+				backgroundColor={'$primary200'}
+				color="$white"
+				onPress={() => router.push('/landing')}
+			>
+				{i18next.t('Back to home')}
+			</Button>
+			<YStack height={48} />
+		</YStack>
 	);
 }
