da26738040b24c0590600d9ae1ca84a4e990aad8
anssi <2003amt@gmail.com>
2025-11-19 18:37:59 +0200

    add fake data for testing

---
+++ prisma.config.ts
@@ -0,0 +1,11 @@
+//copied from https://www.prisma.io/docs/orm/reference/prisma-config-reference
+
+import { defineConfig} from "prisma/config";
+
+export default defineConfig({
+	schema: 'prisma/schema.prisma',
+	migrations: { 
+		path: 'prisma/migrations',
+		seed: 'ts-node prisma/seed.ts',
+	},
+});
