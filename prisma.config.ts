fab4d97b4c732cc0978eb2716821576c492de115
Anssi Timonen <2003amt@gmail.com>
2025-11-24 13:51:03 +0200

    switch to seed to js

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
 	schema: 'prisma/schema.prisma',
 	migrations: {
 		path: 'prisma/migrations',
-		seed: 'ts-node prisma/seed.ts',
+		seed: 'node prisma/seed',
 	},
 });
