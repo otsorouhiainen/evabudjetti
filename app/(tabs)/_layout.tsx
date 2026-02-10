1b4ae65c10bef4c6935d42bbbc7e5b5b995262a4
Mikko Rajakorpi <mikko@mikkorajakorpi.fi>
2026-02-10 11:34:40 +0200

    Add visual feedback for currently selected tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,49 +1,60 @@
-import { Tabs } from "expo-router";
-import type { ReactElement } from "react";
 import { ChartColumn, Home, PlusCircle, Wallet } from '@tamagui/lucide-icons';
+import { Tabs } from 'expo-router';
+import type { ReactElement } from 'react';
+import { useTheme } from 'tamagui';
 
 interface BottomNavOption {
 	icon: ReactElement;
-    name: string;
+	name: string;
 	route: string;
 }
 
-const bottomNavOptions: BottomNavOption[] = [
-	{
-		icon: <Home size={'$icons.md'} />,
-        name: 'Home',
-		route: 'index',
-	},
-	{
-		icon: <PlusCircle size={'$icons.md'} />,
-        name: 'Add transaction',
-		route: 'add_transaction',
-	},
-	{
-		icon: <Wallet size={'$icons.md'} />,
-        name: 'Budget',
-		route: 'budget',
-	},
-	{
-		icon: <ChartColumn size={'$icons.md'} />,
-        name: 'Summary',
-		route: 'summary',
-	},
-];
-
 export default function TabLayout() {
-    return (
-        <Tabs>
-            {bottomNavOptions.map((page) => (
-                <Tabs.Screen
-                    key={page.route}
-                    name={page.route}
-                    options={{
-                        title: page.name,
-                        tabBarIcon: () => page.icon
-                    }}
-                />
-            ))}
-        </Tabs>
-    )
+	const theme = useTheme();
+
+	const bottomNavOptions: BottomNavOption[] = [
+		{
+			icon: <Home size={'$icons.md'} color={theme.white.val} />,
+			name: 'Home',
+			route: 'index',
+		},
+		{
+			icon: <PlusCircle size={'$icons.md'} color={theme.white.val} />,
+			name: 'Add transaction',
+			route: 'add_transaction',
+		},
+		{
+			icon: <Wallet size={'$icons.md'} color={theme.white.val} />,
+			name: 'Budget',
+			route: 'budget',
+		},
+		{
+			icon: <ChartColumn size={'$icons.md'} color={theme.white.val} />,
+			name: 'Summary',
+			route: 'summary',
+		},
+	];
+	return (
+		<Tabs
+			screenOptions={{
+				tabBarStyle: { backgroundColor: theme.primary100.val },
+				tabBarActiveBackgroundColor: theme.primary200.val,
+				tabBarLabelStyle: { color: theme.white.val },
+			}}
+		>
+			{bottomNavOptions.map((page) => (
+				<Tabs.Screen
+					key={page.route}
+					name={page.route}
+					options={{
+						title: page.name,
+						tabBarIcon: () => page.icon,
+						tabBarIconStyle: {
+							color: theme.primary300.val,
+						},
+					}}
+				/>
+			))}
+		</Tabs>
+	);
 }
