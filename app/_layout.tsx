fb841f902957dd6029c895ca07469e8225ebe404
Mikko Rajakorpi <mikko@mikkorajakorpi.fi>
2026-02-10 13:07:34 +0200

    Remove unused import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,29 +2,28 @@ import { TamaguiProvider, Theme } from '@tamagui/core';
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { PortalProvider } from 'tamagui';
-import { BottomNav } from '@/src/components/BottomNav';
 import config from '../tamagui.config';
 
 export default function RootLayout() {
-	return (
-		<TamaguiProvider config={config} defaultTheme={'light'}>
-			{/* PortalProvider is neseccary for Tamagui Dialog components */}
-			<PortalProvider>
-				<Theme name={'light'}>
-					<SafeAreaProvider>
-						<Stack>
-							<Stack.Screen
-								name="(tabs)"
-								options={{ headerShown: false }}
-							/>
-							<Stack.Screen
-								name="modal"
-								options={{ presentation: 'modal' }}
-							/>
-						</Stack>
-					</SafeAreaProvider>
-				</Theme>
-			</PortalProvider>
-		</TamaguiProvider>
-	);
+  return (
+    <TamaguiProvider config={config} defaultTheme={'light'}>
+      {/* PortalProvider is neseccary for Tamagui Dialog components */}
+      <PortalProvider>
+        <Theme name={'light'}>
+          <SafeAreaProvider>
+            <Stack>
+              <Stack.Screen
+                name="(tabs)"
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="modal"
+                options={{ presentation: 'modal' }}
+              />
+            </Stack>
+          </SafeAreaProvider>
+        </Theme>
+      </PortalProvider>
+    </TamaguiProvider>
+  );
 }
