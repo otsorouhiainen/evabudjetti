bfd898d76619dd5540c8173fd5ebbd1ad222d1a1
Riku Puolanne <riku.puolanne@tuni.fi>
2026-02-16 15:06:09 +0200

    change in formatting

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    testPathIgnorePatterns: [
-        '/e2e/',
-        '/home.spec.ts/',
-    ],
+	testPathIgnorePatterns: ['/e2e/', '/home.spec.ts/'],
 };
 
 export default config;
