d9c02ebcc5007d5fe46b4e57cf0c43d4a53734fa
Elias Poikonen <elias.poikonen@tuni.fi>
2026-03-12 16:52:36 +0200

    Added option to toggle reoccurrence on/off, changed biome lineEnding config to 'auto'.

--- src/components/AddItemPopup.tsx
+++ src/components/AddItemPopup.tsx
@@ -43,6 +43,7 @@ const AddItemPopup = ({ onAdd, onClose }: AddItemPopupProps) => {
 		Number(amount) <= 0 ||
 		!startDate;
 	const hasEndDate = endDate !== null;
+	const hasReocurrence = reoccurenceInterval !== undefined;
 	const handleAdd = () => {
 		onAdd({
 			categoryId: 0,
@@ -100,155 +101,177 @@ const AddItemPopup = ({ onAdd, onClose }: AddItemPopupProps) => {
 						/>
 					</View>
 					<View style={styles.singleItemContainer}>
-						<SizableText color="$primary100" size="$title3">
-							{t('Reoccurrence')}
-						</SizableText>
-						<View
+						<XStack
 							style={{
-								flexDirection: 'row',
-								gap: 8,
 								alignItems: 'center',
-								width: '100%',
-								height: '60%',
-								flexWrap: 'wrap',
+								gap: 5,
+								marginBottom: 5
 							}}
 						>
-							<XStack gap={5} alignItems="unset">
-								<Input
-									color="$primary100"
-									textAlign="center"
-									maxLength={3}
-									borderColor={'$primary100'}
-									style={{
-										minWidth: '21%',
-										height: '100%',
-									}}
-									defaultValue="1"
-									keyboardType="numeric"
-									onChangeText={(text) => {
-										const interval = Number(text);
-										if (
-											!Number.isNaN(interval) &&
-											interval > 0
-										) {
-											setReoccurenceInterval(interval);
-										}
-									}}
-								/>
-								<View>
-									<Select
-										disablePreventBodyScroll
-										native="web"
-										defaultValue="month"
-									>
-										<Select.Trigger
-											borderColor={'$primary100'}
-											height={'45'}
-											/* Border style changes when selection is active */
-											borderWidth={recSelect ? 2 : 1}
-											borderBottomLeftRadius={
-												recSelect ? 0 : 15
+							<SizableText color="$primary100" size="$title3">
+								{t('Reoccurrence')}
+							</SizableText>
+							<Checkbox
+								onCheckedChange={() =>
+									hasReocurrence
+										? setReoccurenceInterval(undefined)
+										: setReoccurenceInterval(1)
+								}
+								size="$10"
+							>
+								<Checkbox.Indicator>
+									<Check />
+								</Checkbox.Indicator>
+							</Checkbox>
+						</XStack>
+						{ hasReocurrence && (
+							<View
+								style={{
+									flexDirection: 'row',
+									gap: 8,
+									alignItems: 'center',
+									width: '100%',
+									height: '60%',
+									flexWrap: 'wrap',
+								}}
+							>
+								<XStack gap={5} alignItems="unset">
+									<Input
+										color="$primary100"
+										textAlign="center"
+										maxLength={3}
+										borderColor={'$primary100'}
+										style={{
+											minWidth: '21%',
+											height: '100%',
+										}}
+										defaultValue="1"
+										keyboardType="numeric"
+										onChangeText={(text) => {
+											const interval = Number(text);
+											if (
+												!Number.isNaN(interval) &&
+												interval > 0
+											) {
+												setReoccurenceInterval(interval);
 											}
-											borderBottomRightRadius={
-												recSelect ? 0 : 15
-											}
-											iconAfter={ChevronDown}
-											onPressOut={() =>
-												recSelect
-													? toggleRecSelect(false)
-													: toggleRecSelect(true)
-											}
+										}}
+									/>
+									<View>
+										<Select
+											disablePreventBodyScroll
+											native="web"
+											defaultValue={reoccurence}
 										>
-											<Select.Value
-												color={'$primary100'}
-											/>
-										</Select.Trigger>
+											<Select.Trigger
+												borderColor={'$primary100'}
+												height={'45'}
+												/* Border style changes when selection is active */
+												borderWidth={recSelect ? 2 : 1}
+												borderBottomLeftRadius={
+													recSelect ? 0 : 15
+												}
+												borderBottomRightRadius={
+													recSelect ? 0 : 15
+												}
+												iconAfter={ChevronDown}
+												onPressOut={() =>
+													recSelect
+														? toggleRecSelect(false)
+														: toggleRecSelect(true)
+												}
+											>
+												<Select.Value
+													color={'$primary100'}
+												/>
+											</Select.Trigger>
 
-										<Select.Content>
-											<Select.Viewport>
-												<Select.Group>
-													{REOCCURENCE_OPTIONS.map(
-														(opt, i) => (
-															<Select.Item
-																/* Manually hide and disable the dropdown menu when necessary.
-																This is not optimal and should probably be changed at some point*/
-																opacity={
-																	recSelect
-																		? 100
-																		: 0
-																}
-																disabled={
-																	!recSelect
-																}
-																zIndex={
-																	recSelect
-																		? 2000
-																		: 0
-																}
-																borderColor={
-																	'$primary100'
-																}
-																borderWidth={2}
-																borderTopWidth={
-																	0
-																}
-																borderBottomWidth={
-																	i ===
-																	REOCCURENCE_OPTIONS.length -
-																		1
-																		? 2
-																		: 0
-																}
-																alignSelf="center"
-																marginTop={-0.5}
-																index={i}
-																key={opt}
-																value={opt}
-																onTouchEnd={() => {
-																	setReoccurence(
-																		opt,
-																	);
-																	toggleRecSelect(
-																		false,
-																	);
-																}}
-															>
-																<Select.ItemText
-																	color={
+											<Select.Content>
+												<Select.Viewport>
+													<Select.Group>
+														{REOCCURENCE_OPTIONS.map(
+															(opt, i) => (
+																<Select.Item
+																	/* Manually hide and disable the dropdown menu when necessary.
+																	This is not optimal and should probably be changed at some point*/
+																	opacity={
+																		recSelect
+																			? 100
+																			: 0
+																	}
+																	disabled={
+																		!recSelect
+																	}
+																	zIndex={
+																		recSelect
+																			? 2000
+																			: 0
+																	}
+																	borderColor={
 																		'$primary100'
 																	}
-																	alignContent="center"
-																>
-																	{t(
-																		'Rec_' +
+																	borderWidth={2}
+																	borderTopWidth={
+																		0
+																	}
+																	borderBottomWidth={
+																		i ===
+																		REOCCURENCE_OPTIONS.length -
+																			1
+																			? 2
+																			: 0
+																	}
+																	alignSelf="center"
+																	marginTop={-0.5}
+																	index={i}
+																	key={opt}
+																	value={opt}
+																	onTouchEnd={() => {
+																		setReoccurence(
 																			opt,
-																	)}
-																</Select.ItemText>
-																<Select.ItemIndicator marginLeft="10">
-																	<Check
-																		size={
-																			16
+																		);
+																		toggleRecSelect(
+																			false,
+																		);
+																	}}
+																>
+																	<Select.ItemText
+																		color={
+																			'$primary100'
 																		}
-																	/>
-																</Select.ItemIndicator>
-															</Select.Item>
-														),
-													)}
-												</Select.Group>
-											</Select.Viewport>
-										</Select.Content>
-									</Select>
-								</View>
+																		alignContent="center"
+																	>
+																		{t(
+																			'Rec_' +
+																				opt,
+																		)}
+																	</Select.ItemText>
+																	<Select.ItemIndicator marginLeft="10">
+																		<Check
+																			size={
+																				16
+																			}
+																		/>
+																	</Select.ItemIndicator>
+																</Select.Item>
+															),
+														)}
+													</Select.Group>
+												</Select.Viewport>
+											</Select.Content>
+										</Select>
+									</View>
 
-								<Text
-									color="$primary100"
-									fontWeight={'bold'}
-									paddingVertical={10}
-								>
-									{t('Interval')}
-								</Text>
-							</XStack>
-						</View>
+									<Text
+										color="$primary100"
+										fontWeight={'bold'}
+										paddingVertical={10}
+									>
+										{t('Interval')}
+									</Text>
+								</XStack>
+							</View>
+						)}
 					</View>
 					<View style={styles.singleItemContainer}>
 						<SizableText color="$primary100" size="$title3">
