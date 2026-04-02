358859ee9493f4c51dc4b01160f653bc755317c8
Mikko Rajakorpi <mikko@mikkorajakorpi.fi>
2026-04-02 13:38:56 +0300

    Create second introduction page

---
+++ app/introduction.tsx
@@ -0,0 +1,24 @@
+import { useTranslation } from 'react-i18next';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button, Checkbox, SizableText, YStack } from 'tamagui';
+import { useInstructionStore } from '@/src/store/useInstructionStore';
+
+export default function Instruction() {
+	const { t } = useTranslation();
+	const { instructionShown, setInstructionShown } = useInstructionStore();
+
+	return (
+		<SafeAreaView style={{ flex: 1 }} edges={['left', 'right', 'bottom']}>
+			<YStack
+				backgroundColor="$white"
+				paddingTop="$paddingmd"
+				paddingHorizontal={10}
+				alignItems="center"
+				flex={1}
+				gap={30}
+			>
+				<YStack allignItems="center"></YStack>
+			</YStack>
+		</SafeAreaView>
+	);
+}
